6f72174b8dbc782bc89aa2d7873065d8935e55a1
Verkempynck <ruben.verkempynck@wur.nl>
2017-08-31 23:03:07 +0200

    final dpue check

--- DPUE/check_2016_self_discards.R
+++ DPUE/check_2016_self_discards.R
@@ -2,39 +2,28 @@ rm(list = ls())
 library(tidyverse)
 datap <- "D:\\ICES_WG\\wg_IBPTur.27.4\\DPUE\\check\\"
 
+years <- c(2013:2016)
 
-year <- 2013 #change these each time for basefile
-dyear <- 2009
-
-
-effort <- read_csv(paste(datap, "\\", year, "\\effort_trip_self.csv", sep = "")) %>% mutate(kWdays = hpeffort * 0.745699872) %>% 
-  filter(year == dyear)
-
-
-self <- read_csv(paste(datap, "\\", year, "\\nw_disc_trip_ss.csv", sep = ""), guess_max = 40000) %>%
-  filter(SCIENTIFIC_NAME == "Scophthalmus maximus", prog == "self sampling", year == dyear) %>%
+for (year in years){ 
+self <- read_csv(paste(datap, "\\", year, "\\nw_disc_trip_ss.csv", sep = ""), guess_max = 40000)
+dyear <- unique(self$year)
+self <- filter(self, SCIENTIFIC_NAME == "Scophthalmus maximus", prog == "self sampling", year == dyear) %>%
   mutate(tripnr = TRIPNR + 0) %>% 
   group_by(tripnr, metier) %>% 
   summarise(w_total = sum(w_total_trip)) %>% 
   ungroup()
-
+effort <- read_csv(paste(datap, "\\", year, "\\effort_trip_self.csv", sep = "")) %>% mutate(kWdays = hpeffort * 0.745699872) %>% 
+  filter(year == dyear)
 selfs <- left_join(self, effort, by = "tripnr") %>% filter(metier == "TBB_DEF_70-99_0_0") %>% mutate(dpue = w_total / kWdays)
-
 dpue <- summarise(selfs, dpue = mean(dpue), n = n_distinct(tripnr)) %>% mutate(prog = "self")
-
-
 ob <- read_csv(paste(datap, "\\", year, "\\nw_disc_trip_obs.csv", sep = ""), guess_max = 40000) %>%
   filter(SCIENTIFIC_NAME == "Scophthalmus maximus", prog == "self sampling", year == dyear) %>%
   mutate(tripnr = TRIPNR + 0) %>% 
   group_by(tripnr, metier) %>% 
   summarise(w_total = sum(w_total_trip)) %>% 
   ungroup()
-
 obs <- left_join(ob, effort, by = "tripnr") %>% filter(metier == "TBB_DEF_70-99_0_0") %>% mutate(dpue = w_total / kWdays)
- 
 dpue_ob <- summarise(obs, dpue = mean(dpue), n = n_distinct(tripnr)) %>% mutate(prog = "obs")
-
-
 dpue <- full_join(dpue, dpue_ob)
-
 write_csv(dpue, paste(datap, "\\", dyear, "dpue.csv", sep = ""))
+}
